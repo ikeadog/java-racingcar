db1ba7c4986f7878aaaa9b20009245aeef889342
ikeadog <odeco22@gmail.com>
2023-05-02 00:21:55 +0900

    docs : 자동차 경주 게임 구현 기능 정의

---
+++ src/test/java/racing/racingCarTest.java
@@ -0,0 +1,19 @@
+package racing;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Scanner;
+
+public class racingCarTest {
+    @Test
+    void racingCarTest () {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("자동차 대수는 몇 대 인가요?");
+        int carCount = scan.nextInt();
+        System.out.println("시도할 회수는 몇 회 인가요?");
+        int raceCount = scan.nextInt();
+
+        System.out.println("");
+        System.out.println("실행결과");
+    }
+}
